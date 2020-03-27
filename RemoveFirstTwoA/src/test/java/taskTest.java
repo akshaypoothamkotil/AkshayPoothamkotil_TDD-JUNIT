150ec98a87ade0ddbcc4bbef848579cd49224fdd
Akshay P <notakshayp@gmail.com>
2020-03-27 13:48:48 +0530

    Junit Task

--- RemoveFirstTwoA/src/test/java/taskTest.java
+++ RemoveFirstTwoA/src/test/java/taskTest.java
@@ -9,31 +9,26 @@ public class taskTest {
 	removingA obj=new removingA();
 	@Test
 	public void test1() {
-		//removingA.removeAFunction("ABCD")
 		Assert.assertEquals("BCD", obj.removeAFunction("ABCD"));
 	}
 	
 	@Test
 	public void test2() {
-		//removingA.removeAFunction("ABCD")
 		Assert.assertEquals("CD", obj.removeAFunction("AACD"));
 	}
 	
 	@Test
 	public void test3() {
-		//removingA.removeAFunction("ABCD")
 		Assert.assertEquals("BCD", obj.removeAFunction("BACD"));
 	}
 	
 	@Test
 	public void test4() {
-		//removingA.removeAFunction("ABCD")
 		Assert.assertEquals("BBAA", obj.removeAFunction("BBAA"));
 	}
 	
 	@Test
 	public void test5() {
-		//removingA.removeAFunction("ABCD")
 		Assert.assertEquals("BAA", obj.removeAFunction("AABAA"));
 	}
 
